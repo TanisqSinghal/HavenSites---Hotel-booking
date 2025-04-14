94597b990f5a22af2907165d5e8ca05190fe9a24
tanishak_singhal <tanishaksinghal785@gmail.com>
2025-04-14 17:07:43 +0530

    added flash

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate'); // EJS template engine for Express
 const ExpressError = require('./utils/ExpressError'); // Error handling middleware
+const session = require('express-session'); // Session middleware for Express
+const flash = require('connect-flash'); // Flash messages middleware
+
 
 const listings = require('./routes/listing.js'); // Adjust the path as necessary
 const reviews = require('./routes/review.js'); // Adjust the path as necessary
@@ -32,9 +35,31 @@ app.use(methodOverride('_method')); // Middleware to support PUT and DELETE requ
 app.use(express.static(path.join(__dirname, 'public'))); // Middleware to serve static files
 app.engine('ejs', ejsMate); // Use ejsMate for EJS template engine
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use(session(sessionOptions)); // Initialize session middleware
+app.use(flash()); // Initialize flash middleware
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success'); // Flash success messages
+    res.locals.error = req.flash('error'); // Flash error messages
+    // console.log(error);
+    next(); // Proceed to the next middleware or route handler
+});
+
 
 app.use('/listings', listings); // Use the listings router for all routes starting with /listings
 app.use('/listings/:id/reviews', reviews);
--- routes/listing.js
+++ routes/listing.js
@@ -40,6 +40,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.post("/", validateListing, wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash('success', 'Your listing is being added!');
     res.redirect("/listings");
 }));
 
