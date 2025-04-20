968c539a132d37fa2c90fe8bda55ec745c54b22d
tanishak_singhal <tanishaksinghal785@gmail.com>
2025-04-21 01:26:03 +0530

    added codes 4

--- controllers/listings.js
+++ controllers/listings.js
@@ -24,6 +24,7 @@ module.exports.createListing = async (req, res, next) => {
 
     const location = req.body.listing.location;
     const maptiler = await import('@maptiler/sdk');
+
     maptiler.config.apiKey = `cfannnRv2gCRgPct3GGk`; // Replace with your MapTiler API key
 
     const result = await maptiler.geocoding.forward(location,
