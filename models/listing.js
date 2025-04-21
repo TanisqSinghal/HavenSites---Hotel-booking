c2acd9d93ae6bfc7238b9f342c8631c03e69f279
tanishak_singhal <tanishaksinghal785@gmail.com>
2025-04-21 16:47:01 +0530

    added full final project

--- models/listing.js
+++ models/listing.js
@@ -29,7 +29,8 @@ const listingSchema = new Schema({
         type: {
           type: String, // Don't do `{ location: { type: String } }`
           enum: ['Point'], // 'location.type' must be 'Point'
-          required: true
+          required: true,
+          default: 'Point'
         },
         coordinates: {
           type: [Number],
